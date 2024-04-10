f53220321c5fb08733f01adaddb719b0f1d189d0
mielyn <mielynacosta@pursuit.org>
2024-04-09 20:55:01 -0500

    add api key, store in .env, initial chart implementation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import React, { useState } from "react";
 import "./App.css";
 import FinancialChart from "./components/FinancialChart";
 
 function App() {
+  const [symbol, setSymbol] = useState("IBM");
   return (
     <div className="App">
-      <FinancialChart />
+      <h2>Financial Dashboard</h2>
+      <input
+        type="text"
+        value={symbol}
+        onChange={(e) => setSymbol(e.target.value.toUpperCase())}
+        placeholder="Enter Stock Symbol"
+      />
+      <FinancialChart symbol={symbol} />
     </div>
   );
 }
