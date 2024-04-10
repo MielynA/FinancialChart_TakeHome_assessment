22cc169dd5ecb94bf3665afee626d405568d9571
mielyn <mielynacosta@pursuit.org>
2024-04-09 21:29:38 -0500

    add simple css bootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { useState } from "react";
-import "./App.css";
+// import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import FinancialChart from "./components/FinancialChart";
 
 function App() {
   const [symbol, setSymbol] = useState("IBM");
   return (
-    <div className="App">
-      <h2>Financial Dashboard</h2>
-      <input
-        type="text"
-        value={symbol}
-        onChange={(e) => setSymbol(e.target.value.toUpperCase())}
-        placeholder="Enter Stock Symbol"
-      />
+    <div className="App container mt-5">
+      <div className="d-flex flex-column align-items-center">
+        <h2 className="mb-4">Financial Dashboard</h2>
+
+        <input
+          className="mb-5"
+          type="text"
+          value={symbol}
+          onChange={(e) => setSymbol(e.target.value.toUpperCase())}
+          placeholder="Enter Stock Symbol"
+          aria-label="Stock Symbol"
+          aria-describedby="button-addon2"
+        />
+      </div>
       <FinancialChart symbol={symbol} />
     </div>
   );
